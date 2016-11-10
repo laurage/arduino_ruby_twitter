025eafde418a98c8a60c418bcb7fdf7e7438a8c3
Laura Pedroni <laurage_@hotmail.com>
2016-11-10 13:43:52 +0100

    change comments

--- sketch_ledsblink/sketch_ledsblink.ino
+++ sketch_ledsblink/sketch_ledsblink.ino
@@ -2,14 +2,12 @@ int LEDgreen = 13;
 int LEDred = 12;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LEDgreen,OUTPUT);
   pinMode(LEDred,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(LEDgreen, HIGH);
   digitalWrite(LEDred, LOW);
   delay(500);
